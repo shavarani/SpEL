a72115194ff121c29aab279cf694d8337fb85c3e
Hassan Shavarani <sshavara@sfu.ca>
2023-08-24 17:22:36 -0700

    CoT mode/prompt added

--- src/openai_gpt/utils.py
+++ src/openai_gpt/utils.py
@@ -19,9 +19,20 @@ else:
 
 
 spacy_model = spacy.load("en_core_web_sm")
-PROMPT = "Annotate the wikipedia entities in the following paragraph, and " \
-         "produce the output in html markup using the <mark> element and the data-entity attribute:\n\n"
+SIMPLE_PROMPT = ("Annotate the wikipedia entities in the following paragraph, and "
+                 "produce the output in html markup using the <mark> element and the data-entity attribute:\n\n{}")
 
+COT_PROMPT = ("Annotate the Wikipedia entities in the following paragraph, and produce the output in markup using the"
+              " <mark> element and the data-entity attribute:\n\nDocument: \"EU rejects German call to boycott British"
+              " lamb . \"\n\nAnswer: <p><chain-of-thought>Considering EU, German, and British are shown in the text "
+              "together with the word boycott, this is a political document, I should annotate EU with \"European "
+              "Union\",  German with the country \"Germany\", and British with the country \"United Kingdom\". "
+              "I make sure I do not mistake Wikipedia identifiers with entity type identifiers, for example I choose "
+              "\"United Kingdom\" instead of the incorrect general entity type \"country\". I make sure to annotate all"
+              " entities even if there is a large number of entities.</chain-of-thought><result>"
+              "<mark data-entity=\"European Union\">EU</mark> rejects <mark data-entity=\"Germany\">German"
+              "</mark> call to boycott <mark data-entity=\"United Kingdom\">British</mark> lamb .</result></p>\n\n"
+              "Document: \"{}\"")
 
 def num_tokens_from_messages(messages, model_name=GPT_MODEL_NAME):
     assert model_name in ["gpt-3.5-turbo-16k", "gpt-4-0613"], f"requested model name:{model_name} is not supported!"
@@ -39,10 +50,10 @@ def num_tokens_from_messages(messages, model_name=GPT_MODEL_NAME):
     return num_tokens
 
 
-def create_messages(text, prompt=PROMPT):
+def create_messages(text, prompt):
     return [
         {"role": "system", "content": "You are a Wikipedia annotator."},
-        {"role": "user", "content": prompt + text},
+        {"role": "user", "content": prompt.format(text)},
     ]
 
 
@@ -51,7 +62,7 @@ def create_messages(text, prompt=PROMPT):
     openai.error.RateLimitError,
     max_time=240
 )
-def chat_completion(text, prompt=PROMPT, model_name=GPT_MODEL_NAME):
+def chat_completion(text, prompt, model_name=GPT_MODEL_NAME):
     return openai.ChatCompletion.create(
         model=model_name,
         temperature=0,
@@ -63,15 +74,19 @@ def chat_completion(text, prompt=PROMPT, model_name=GPT_MODEL_NAME):
     )
 
 
-def make_chat_completion_query(text, prompt=PROMPT, model_name=GPT_MODEL_NAME):
-    r = chat_completion(text, prompt, model_name)
+def make_chat_completion_query(text, prompt, model_name=GPT_MODEL_NAME):
+    try:
+        r = chat_completion(text, prompt, model_name)
+    except openai.error.APIError:
+        return ""
     annotated = r.choices[0].message['content'].strip()
     if r.choices[0].finish_reason == "length":
         logging.warning("Potentially longer than maximum token, request returned with finish_reason == length")
     return annotated
 
 
-def chunk_and_annotate(context_mention, chunk_size_limit=996):
+def chunk_and_annotate(context_mention, chunk_size_limit=996, use_cot_prompt=True):
+    prompt = COT_PROMPT if use_cot_prompt else SIMPLE_PROMPT
     logging.info('Running chunk_and_annotate on input:')
     logging.info('='*80)
     logging.info(context_mention)
@@ -92,19 +107,22 @@ def chunk_and_annotate(context_mention, chunk_size_limit=996):
         logging.warning("=" * 80)
     sent_index = 0
     while sent_index < len(sentences):
-        while num_tokens_from_messages(create_messages(chunk)) < chunk_size_limit and sent_index < len(sentences):
+        while num_tokens_from_messages(create_messages(chunk, prompt)) < chunk_size_limit and sent_index < len(sentences):
             chunk += sentences[sent_index]
             sent_index += 1
-        logging.info(f"making a query with size: {num_tokens_from_messages(create_messages(chunk))} tokens!")
-        result += make_chat_completion_query(chunk)
+        logging.info(f"making a query with size: {num_tokens_from_messages(create_messages(chunk, prompt))} tokens!")
+        result += make_chat_completion_query(chunk, prompt)
         chunk = ""
-    result = '<p>' + result + '</p>'
+    if not use_cot_prompt:
+        result = '<p>' + result + '</p>'
     logging.info('received the final result from openai gpt model:')
     logging.info('='*80)
     logging.info(result)
     logging.info('='*80)
     soup = BeautifulSoup(result, 'html.parser')
-    markups = soup.find_all('mark')
+    if use_cot_prompt:
+        soup = soup.find('result')
+    markups = soup.find_all('mark') if soup is not None else []
     final_results = []
     current_index = 0
 
